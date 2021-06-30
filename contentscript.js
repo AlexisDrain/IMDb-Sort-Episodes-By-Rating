93a5616e769e75e7bf99d5039cbd8db657722b44
JesseDrain <JesseDrain97@yahoo.com>
2021-06-30 12:37:19 -0400

    Initial version for Chrome

--- contentscript.js
+++ contentscript.js
@@ -1,23 +1,19 @@
 // ==UserScript==
 // @name            IMDb Sort Episodes by Rating
-// @description     This script adds a button to sort tv show episodes by rating on IMDB. 
+// @description     This script adds a button to sort tv show episodes by rating on IMDb 
 // @version         1
 // @match           *://www.imdb.com/title/tt*
 // @grant           none
-// @namespace https://greasyfork.org/users/719286
 // ==/UserScript==
 
 
-
 function checkButtonWasNotDeleted() {
-  console.log("check");
   if(!document.getElementById("Sort by Rating")){
     addButton('Sort by Rating', sortByRating)
   }
 }
 
 function addButton(text, onclick, cssObj) {
-    console.log("addButton");
     cssObj = cssObj || {"float":"right", "margin-right":"1em", "font-size": "0.50em", 'z-index': 3}
     let button = document.createElement('button'), btnStyle = button.style
     document.getElementById("episode_top").appendChild(button)
@@ -28,21 +24,43 @@ function addButton(text, onclick, cssObj) {
     return button
 }
 
+
+
 function sortByRating() {
-    parent = document.getElementsByClassName('list detail eplist')[0]
-  
-    for(var cardChild=parent.firstChild; cardChild!==null; cardChild=cardChild.nextSibling) {
-      if(!cardChild.className){
-        continue;
-      }
-      cardChild.cardValue = getDescendantWithClass(cardChild,"ipl-rating-star__rating").textContent;
-      console.log(cardChild);
-      console.log(cardChild.cardValue);
+  buttonText = document.getElementById("Sort by Rating").innerHTML;
+  if(buttonText == "Sort by Rating") {
+    document.getElementById("Sort by Rating").innerHTML = "Sort Chronologically";
+  } else {
+    document.getElementById("Sort by Rating").innerHTML = "Sort by Rating";
+  }
+
+  parent = document.getElementsByClassName('list detail eplist')[0]
+
+  var i = 1;
+  for(var cardChild=parent.firstChild; cardChild!==null; cardChild=cardChild.nextSibling) {
+    if(!cardChild.className){
+      continue;
     }
+
+    if(buttonText == "Sort by Rating") {
+      cardChild.cardValue = getDescendantWithClass(cardChild,"ipl-rating-star__rating").textContent;
+      cardChild.chronologicalOrder = i;
+      i += 1;
+    } else { // sort chronologically
+      cardChild.cardValue = cardChild.chronologicalOrder;
+    }
+
+  }
   
-  [...parent.children]
-  .sort((a,b)=>a.cardValue<b.cardValue?1:-1)
-  .forEach(node=>parent.appendChild(node));
+  if(buttonText == "Sort by Rating") {
+    [...parent.children]
+    .sort((a,b)=>a.cardValue<b.cardValue?1:-1)
+    .forEach(node=>parent.appendChild(node));
+  } else { // sort chronologically
+    [...parent.children]
+    .sort((a,b)=>a.cardValue>b.cardValue?1:-1)
+    .forEach(node=>parent.appendChild(node));
+  }
 }
 
 function getDescendantWithClass(element, clName) {
@@ -61,8 +79,6 @@ function getDescendantWithClass(element, clName) {
       }
       return null;
 }
-
-console.log("hello page");
 
 addButton('Sort by Rating', sortByRating)
 setInterval(checkButtonWasNotDeleted, 2000);
