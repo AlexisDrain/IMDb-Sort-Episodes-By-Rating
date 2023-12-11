b6a9ac30571c087ec6d44b721c4c9e655eb86424
Alexis Drain <alexisdrain97@gmail.com>
2023-12-10 17:50:52 -0800

    fixed sorting. update to manifest v3

--- contentscript.js
+++ contentscript.js
@@ -6,79 +6,100 @@
 // @grant           none
 // ==/UserScript==
 
+const regex_GetNum = /\d+\.\d+/;
+var mainCheckbox;
 
 function checkButtonWasNotDeleted() {
-  if(!document.getElementById("Sort by Rating")){
-    addButton(sortByRating)
+  if(document.getElementById("Sort-by-Rating") === null){
+    console.log("Button not found: add button");
+    addSortButton();
   }
 }
 
-function addButton(onclick, cssObj) {
-    cssObj = cssObj || {"float":"right", "margin-right":"1em", "font-size": "0.50em", 'z-index': 3}
-    let button = document.createElement('button'), btnStyle = button.style
-    document.getElementById("episode_top").appendChild(button)
-    button.innerHTML = 'Sort by Rating'
-    button.id = 'Sort by Rating'
-    button.onclick = onclick
-    Object.keys(cssObj).forEach(key => btnStyle[key] = cssObj[key])
-    return button
+function addSortButton() {
+
+  var div = document.createElement('div');
+  div.id = "Sort-by-Rating-Div";
+  div.style.backgroundColor = "#ebebeb";
+  div.style.borderRadius = "5px";
+  div.style.overflow = "hidden";
+  document.querySelector(".sc-58f3e8aa-0.cLMIyf").prepend(div)
+
+  var smallYellow = document.createElement("span");
+  smallYellow.role = "presentation";
+  smallYellow.style.backgroundColor = "#f5c518";
+  smallYellow.style.display = "inline-block";
+  smallYellow.style.width = "4px";
+  smallYellow.style.height = "100px";
+  div.appendChild(smallYellow);
+
+  mainCheckbox = document.createElement('input')
+  mainCheckbox.type = "checkbox";
+  mainCheckbox.innerHTML = 'Sort by Rating'
+  mainCheckbox.id = 'Sort-by-Rating'
+  mainCheckbox.onchange = sortByRating;
+  div.appendChild(mainCheckbox);
+
+  var label = document.createElement("label");
+  label.id = 'Sort-by-Rating-Label'
+  label.innerText = " Sort by Rating";
+  label.htmlFor = mainCheckbox.id;
+  div.appendChild(label);
 }
 
-
+var currentlySortedByRating = false;
 
 function sortByRating() {
-  buttonText = document.getElementById("Sort by Rating").innerHTML;
-  if(buttonText == "Sort by Rating") {
-    document.getElementById("Sort by Rating").innerHTML = "Sort Chronologically";
-  } else {
-    document.getElementById("Sort by Rating").innerHTML = "Sort by Rating";
-  }
-
-  parent = document.getElementsByClassName('list detail eplist')[0]
+  
+  parent = document.querySelector(".sc-58f3e8aa-0.cLMIyf");
+  
+  var sortButton = document.querySelector("#Sort-by-Rating");
+  currentlySortedByRating = !currentlySortedByRating; // toggle
+  
+  mainCheckbox.checked = currentlySortedByRating;
 
   var i = 1;
-  for(var cardChild=parent.firstChild; cardChild!==null; cardChild=cardChild.nextSibling) {
-    if(!cardChild.className){
+  for (var cardChild=parent.firstChild; cardChild!==null; cardChild=cardChild.nextSibling) {
+    
+    if (cardChild.id == "Sort-by-Rating-Div") {
       continue;
     }
-
-    if(buttonText == "Sort by Rating") {
-      cardChild.cardValue = getDescendantWithClass(cardChild,"ipl-rating-star__rating").textContent;
+    
+    cardChild.cardAriaLabel = cardChild.querySelector("[aria-label^='IMDb rating: ']").ariaLabel;
+    cardChild.cardValue = cardChild.cardAriaLabel.match(regex_GetNum);
+    
+    if (currentlySortedByRating) {
+      // cardChild.cardValue = getDescendantWithClass(cardChild,"ipl-rating-star__rating").textContent;
       cardChild.chronologicalOrder = i;
       i += 1;
     } else { // sort chronologically
       cardChild.cardValue = cardChild.chronologicalOrder;
     }
-
+    
   }
   
-  if(buttonText == "Sort by Rating") {
+  if(currentlySortedByRating) {
     [...parent.children]
     .sort((a,b)=>a.cardValue<b.cardValue?1:-1)
     .forEach(node=>parent.appendChild(node));
-  } else { // sort chronologically
+  } else if(currentlySortedByRating == false) { // sort chronologically
     [...parent.children]
     .sort((a,b)=>a.cardValue>b.cardValue?1:-1)
     .forEach(node=>parent.appendChild(node));
   }
 }
 
-function getDescendantWithClass(element, clName) {
-    var children = element.childNodes;
-    for (var i = 0; i < children.length; i++) {
-        if (children[i].className &&
-            children[i].className.split(' ').indexOf(clName) >= 0) {
-            return children[i];
-          }
-      }
-      for (var i = 0; i < children.length; i++) {
-          var match = getDescendantWithClass(children[i], clName);
-          if (match !== null) {
-              return match;
-          }
-      }
-      return null;
-}
+// Refresh page when season button is clicked
+ var seasonBar = document.querySelector(".sc-bb8e8454-0.hshVlp");
+const observer = new MutationObserver(refreshPage);
+observer.observe(seasonBar, { attributes: true, childList: true, subtree: true });
 
-addButton('Sort by Rating', sortByRating)
-setInterval(checkButtonWasNotDeleted, 2000);
+ function refreshPage() {
+   if(currentlySortedByRating == true) {
+     sortByRating();
+   }
+ }
+ 
+ // Init page
+ addSortButton();
+ setInterval(checkButtonWasNotDeleted, 2000);
